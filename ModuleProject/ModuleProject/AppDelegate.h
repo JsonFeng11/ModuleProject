ac1e58b872ee60060195cec856ed9a1982821548
JsonFeng <json_feng@outlook.com>
2017-01-02 13:42:50 +0800

    初始化工程

---
+++ ModuleProject/ModuleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleProject
+//
+//  Created by fdd on 17/1/2.
+//  Copyright © 2017年 fdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModuleProject/ModuleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleProject
+//
+//  Created by fdd on 17/1/2.
+//  Copyright © 2017年 fdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
